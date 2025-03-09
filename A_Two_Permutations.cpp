cebbbe6db38f9837129ce880c384fdffd57a07a3
arjunbothra <arjunbothra78@gmail.com>
2025-03-09 14:28:26 +0530

    questions solved

--- A_Two_Permutations.cpp
+++ A_Two_Permutations.cpp
@@ -9,6 +9,10 @@ int main(){
     int t;
     cin >> t;
     while (t--){
+        int n, a, b;
+        cin >> n >> a >> b;
+        if(n-(a+b)>=2 || a==n && a==b) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
     }
     return 0;
 }
